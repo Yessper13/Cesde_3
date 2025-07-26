c85eb0f1c6c55eb07ff12206ee4cf2f8178e54fb
yessper13 <yessper13@gmail.com>
2025-07-26 08:46:29 -0500

    actualizacion de carpeta, con api rest

---
+++ Backend/ClassRoomAppS/src/test/java/com/example/ClassRoomAppS/ClassRoomAppSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ClassRoomAppS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassRoomAppSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
